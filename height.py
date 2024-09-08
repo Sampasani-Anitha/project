8facf6d59a3e6ce7e829af1804ba49951d4fab4d
Sampasani-Anitha <sampasanianitha@gmail.com>
2024-09-08 14:59:31 +0530

    adding this file

---
+++ height.py
@@ -0,0 +1,12 @@
+print("welcome to the roller coster")
+height=int(input("what is your height cm?")
+bill=0
+if(height>=120):
+           print("you can ride the roller coaster")
+           
+else:      printf("you have to grow taller")
+if age<18:
+    
+           printf("you have to pay $5")
+else:
+           prntf("you have to pay $7")
